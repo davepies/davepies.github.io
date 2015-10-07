151bd7d736e82e2ba3bd736f1e16c0021b0e5fee
Dave Pisek <dave.pisek@gmail.com>
2015-10-08 03:15:50 +1100

    Minifies css for prod.

--- gulpfile.js
+++ gulpfile.js
@@ -3,13 +3,23 @@
 const browserSync = require('browser-sync'),
     gulp = require('gulp'),
     jade = require('gulp-jade'),
+    minimist = require('minimist'),
     postcss = require('gulp-postcss'),
 
     paths = {
         styles: 'styles/main.css',
         templates: 'templates/*.jade'
     },
-    out = 'build/';
+    out = 'build/',
+
+    knownOptions = {
+        string: 'env',
+        default: { env: process.env.NODE_ENV || 'production'}
+    },
+    options = minimist(process.argv.slice(2), knownOptions),
+
+    isProdBuild = options.env === 'production';
+
 
 // register tasks
 gulp.task(styles);
@@ -17,6 +27,8 @@ gulp.task(templates);
 
 gulp.task('build', gulp.parallel(templates, styles));
 gulp.task('serve', gulp.series('build', watch));
+gulp.task('default', gulp.series('build'));
+
 
 // tasks
 function watch() {
@@ -38,18 +50,28 @@ function templates() {
 }
 
 function styles() {
+    const commonPlugins = [
+            require('postcss-import'),
+            require('cssnext')(),
+            require('postcss-nested'),
+            require('csstyle'),
+            require('postcss-extend'),
+            require('postcss-color-function')
+        ],
+        prodPlugins = [
+            require('cssnano')({
+                discardComments: { removeAll: true }
+            })
+        ];
+
+    var pluginsToRun = commonPlugins;
+
+    if (isProdBuild) {
+        pluginsToRun = commonPlugins.concat(prodPlugins);
+    }
+
     return gulp.src(paths.styles)
-               .pipe(postcss([
-                   require('postcss-import'),
-                   require('cssnext')(),
-                   require('postcss-nested'),
-                   require('csstyle'),
-                   require('postcss-extend'),
-                   require('postcss-color-function'),
-                //    require('cssnano')({
-                //        discardComments: { removeAll: true }
-                //    })
-               ]))
+               .pipe(postcss(pluginsToRun))
                .pipe(gulp.dest(out))
                .pipe(browserSync.stream());
 }
