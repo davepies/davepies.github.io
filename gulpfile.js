2fd066d94b5f586fc7fa9e90e8bbb683e5d200ad
Dave Pisek <dave.pisek@gmail.com>
2015-10-09 02:33:34 +1100

    Adds footer icons.

--- gulpfile.js
+++ gulpfile.js
@@ -3,33 +3,47 @@
 
 'use strict';
 
-const browserSync = require('browser-sync'),
-    gulp = require('gulp'),
-    jade = require('gulp-jade'),
-    minimist = require('minimist'),
-    postcss = require('gulp-postcss'),
+const path = require('path'),
+      browserSync = require('browser-sync'),
+      gulp = require('gulp'),
+      jade = require('gulp-jade'),
+      minimist = require('minimist'),
+      postcss = require('gulp-postcss'),
 
-    paths = {
-        styles: 'styles/main.css',
-        templates: 'templates/*.jade'
-    },
-    out = 'build/',
+      paths = {
+          styles: 'styles/main.css',
+          templates: 'templates/index.jade',
+          svg: 'assets/svg/*.svg'
+      },
+      watchPaths = {
+          templates: 'templates/**/*.jade',
+          styles: 'styles/**/*.css'
+      },
+      out = 'build/',
 
-    knownOptions = {
-        string: 'env',
-        default: { env: process.env.NODE_ENV || 'production'}
-    },
-    options = minimist(process.argv.slice(2), knownOptions),
+      knownOptions = {
+          string: 'env',
+          default: { env: process.env.NODE_ENV || 'production'}
+      },
+      options = minimist(process.argv.slice(2), knownOptions),
 
-    isProdBuild = options.env === 'production';
+      isProdBuild = options.env === 'production';
 
 
 // register tasks
+gulp.task(clean);
 gulp.task(styles);
 gulp.task(templates);
+gulp.task(svg);
 
-gulp.task('build', gulp.parallel(templates, styles));
+gulp.task('build', gulp.series(
+    clean,
+    gulp.parallel(templates, styles),
+    svg
+));
+
 gulp.task('serve', gulp.series('build', watch));
+
 gulp.task('default', gulp.series('build'));
 
 
@@ -37,11 +51,44 @@ gulp.task('default', gulp.series('build'));
 function watch() {
     browserSync.init({ server: './build' });
 
-    gulp.watch('styles/**/*.css', styles);
-    gulp.watch(paths.templates, templates)
+    gulp.watch(watchPaths.styles, styles);
+    gulp.watch(watchPaths.templates, gulp.series(templates, svg))
         .on('change', function () {
             setTimeout(browserSync.reload, 0.5e3);
         });
+}
+
+function clean() {
+    return require('del')(['build']);
+}
+
+function svg() {
+    let svgs = gulp.src(paths.svg)
+                   .pipe(require('gulp-svgmin')(function (file) {
+                       let prefix = path.basename(
+                           file.relative,
+                           path.extname(file.relative)
+                       );
+                       return {
+                           plugins: [{
+                               cleanupIDs: {
+                                   prefix: prefix + '-',
+                                   minify: true
+                               }
+                           }]
+                       };
+                   }))
+                   .pipe(require('gulp-svgstore')({
+                       inlineSvg: true
+                   }));
+
+    return gulp.src(out + 'index.html' )
+               .pipe(require('gulp-inject')(svgs, {
+                   transform: function (_, file) {
+                       return file.contents.toString();
+                   }
+               }))
+               .pipe(gulp.dest(out));
 }
 
 function templates() {
